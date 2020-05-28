855df43111cb9897bd399849e39ebd0e7d8264d7
Cyril Raffatelli <exha@exha.moe>
2020-05-28 12:56:55 +0200

    Add @tryghost/admin-api, add /add_point endpoint authors logging

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const app_port = process.env.APP_PORT || 1337;
+const GhostAdminAPI = require('@tryghost/admin-api');
+
+const ghost_admin_api = new GhostAdminAPI({
+    url: process.env.GHOST_APP_URL,
+    key: process.env.ADMIN_API_KEY,
+    version: 'v3'
+});
 
 app.listen(app_port, () => {
     console.log('Running on port ' + app_port);
@@ -10,5 +17,13 @@ app.listen(app_port, () => {
 
 app.post('/add_point', (req, res, next) => {
     console.log('add_point endpoint triggered');
+    next();
+}, async (req, res) => {
+    const latest_post = await ghost_admin_api.posts.browse({limit: 1});
+    const post_authors = latest_post[0].authors;
+
+    post_authors.forEach(el => {
+        console.log('Adding one point to user ' + el.name + ' (' + el.id + ')');
+    });
     res.sendStatus(200);
 });
