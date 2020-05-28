6cd06f425b5d3ffebd05e7df9149dff2f4d6a487
Cyril Raffatelli <exha@exha.moe>
2020-05-28 10:46:57 +0200

    Add app.js skeleton

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
+const app_port = process.env.APP_PORT || 1337;
+
+app.listen(app_port, () => {
+    console.log('Running on port ' + app_port);
+});
