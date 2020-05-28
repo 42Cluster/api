434d2e5051d936cbc324ea5836ceda14616618d1
Cyril Raffatelli <exha@exha.moe>
2020-05-28 10:49:44 +0200

    Add /add_point endpoint POST handling

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ const app_port = process.env.APP_PORT || 1337;
 app.listen(app_port, () => {
     console.log('Running on port ' + app_port);
 });
+
+app.post('/add_point', (req, res, next) => {
+    console.log('add_point endpoint triggered');
+    res.sendStatus(200);
+});
